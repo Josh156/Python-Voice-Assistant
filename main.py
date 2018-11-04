71d8dbe81617bf60ac9a8b25af3bc01072d2aaa6
huhehu <robloxvr21@gmail.com>
2018-11-04 19:26:37 +0100

    --

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def listen():
     with sr.Microphone() as source:
         print("Listening...")
         audio = r.listen(source)
-        r.energy_threshold = 
+        r.energy_threshold(4000) #excuse me u fucking bitch?
         try:
             userSpoke(r.recognize_google(audio))
         except:
