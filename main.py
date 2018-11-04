306363b0c72e8dedf5df594f4ee13cc121821c2b
huhehu <robloxvr21@gmail.com>
2018-11-04 19:32:54 +0100

    removed energy thershold as its being bitchy

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def listen():
     with sr.Microphone() as source:
         print("Listening...")
         audio = r.listen(source)
-        r.energy_threshold(4100) #excuse me u fucking bitch?
+        r.energy_threshold(4500) #excuse me u fucking bitch?
         try:
             userSpoke(r.recognize_google(audio))
         except:
