98ecac44962cb8103ec8d0ee07d5dae66e824f16
Josh156 <44727842+Josh156@users.noreply.github.com>
2018-11-04 13:59:11 +0000

    Fixed up time

--- main.py
+++ main.py
@@ -13,21 +13,37 @@ def tts(string):
 #userspoke function
 def userSpoke(speech):
     print(speech)
-    if speech == "what's the time": #try to make this say things like 5 past 1
-        theTime = time.localtime()#[0, 0, 0, 1, 0]
+    #try to make this say things like 5 past 1
+    if speech == "what's the time" or speech == "what is the time":
+        theTime = time.localtime()#[0, 0, 0, 1, 15]
+
+        #convert military time
         if theTime[3] > 12:
             hour = str(theTime[3] - 12)
         else:
             hour = str(theTime[3])
-        if theTime[4] < 10:
-            if theTime[4] == 0:
-                minute = "o'clock"
+
+        #decide past or to
+        minute = theTime[4]
+        if minute > 30:
+            hour = str(int(hour) + 1)
+            if minute == 45:
+                arguments = "quarter to " + hour
             else:
-                minute = "0" + str(theTime[4])
+                arguments = str(60 - minute) + " minutes to " + hour
         else:
-            minute = str(theTime[4])
-        tts("The time is " + hour + " " + minute)
-        print(hour, minute)
+            if minute == 0:
+                arguments = hour + " o'clock"
+            elif minute == 15:
+                arguments = "quarter past " + hour
+            elif minute == 30:
+                arguments = "half past " + hour
+            else:
+                arguments = str(minute) + " minutes past " + hour
+
+        #send arguments to function
+        tts("The time is " + arguments)
+        print(arguments)
 
 #speech recognition
 def listen():
@@ -35,14 +51,17 @@ def listen():
     with sr.Microphone() as source:
         print("Listening...")
         audio = r.listen(source)
-        #try:
-        userSpoke(r.recognize_google(audio))
-        #except:
-            #tts("I couldn't quite catch that")
+        try:
+            userSpoke(r.recognize_google(audio))
+        except:
+            tts("I couldn't quite catch that")
 
 #wait for the user to give microphone permission
 #later we will change this to a phrase like "okay, python"
-while True:
+userSpoke("what's the time")
+"""while True:
     userInput = input("Would you like me to listen? (y/n)\n")
     if userInput == "y":
         listen()
+    if userInput == "n":
+        quit()"""
