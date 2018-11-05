a3b7f355164bf05f1ee8cb4effb26d4997134ba3
huhehu <robloxvr21@gmail.com>
2018-11-05 18:51:00 +0100

    Added different file in which we defined functions

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ try:
 except:
     print("Please install SpeechRecognition and PyAudio!")
 
+try:
+    from bs4 import BeautifulSoup
+except:
+    print("Please install bs4")
+
 #Start up TTS libraries.
 speak = Dispatch("SAPI.SpVoice")
 def tts(string):
@@ -20,28 +25,7 @@ def userSpoke(speech):
     print(speech)
     #Time
     if "what's the time" in speech or "what is the time" in speech:
-        theTime = time.localtime()
-        if theTime[3] > 12:
-            hour = str(theTime[3] - 12)
-        else:
-            hour = str(theTime[3])
-        minute = theTime[4]
-        if minute > 30:
-            hour = str(int(hour) + 1)
-            if minute == 45:
-                arguments = "quarter to " + hour
-            else:
-                arguments = str(60 - minute) + " minutes to " + hour
-        else:
-            if minute == 0:
-                arguments = hour + " o'clock"
-            elif minute == 15:
-                arguments = "quarter past " + hour
-            elif minute == 30:
-                arguments = "half past " + hour
-            else:
-                arguments = str(minute) + " minutes past " + hour
-        tts("The time is " + arguments)
+        
     #Search Online
     elif speech.startswith("search for"):
         searchTerm = speech.split()[2:]
@@ -49,10 +33,15 @@ def userSpoke(speech):
         if speech.endswith("online"):
             webbrowser.open("https://www.google.com/search?q=" + quote(" ".join(searchTerm[:-1])))
         elif speech.endswith("on youtube"):
-            tts("This feature is coming soon!")
+            #tts("This feature is coming soon!")
+
+            print("Lotta testing")
     #Some Fortnite meme
     elif speech == "fortnite" or speech == "fortnight":
         tts("Fortnite players are virgins, by the way fortnight dances are pretty cool!")
+    #github repo
+    elif speech == "open your github repository":
+        webbrowser.open("https://github.com/Josh1560/Python-Voice-Assistant")
 
 #Does the SpeechRecognition stuff
 def listen():
