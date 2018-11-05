49cc1765acb46fedf63abb23c85aba393337f85e
huhehu <robloxvr21@gmail.com>
2018-11-05 15:06:34 +0100

    shh3

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def userSpoke(speech):
         elif speech.endswith("on youtube"):
             tts("This feature is coming soon!")
     #Some Fortnite meme
-    elif "fortnite" in speech:
+    elif speech == "fortnite" or speech == "fortnite":
         tts("Fortnite players are virgins, by the way fortnight dances are pretty cool!")
 
 #Does the SpeechRecognition stuff
